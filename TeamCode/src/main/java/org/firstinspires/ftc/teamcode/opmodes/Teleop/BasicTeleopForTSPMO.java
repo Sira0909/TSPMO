df3304ce92e5869351423e3ce1ef521726f463e6
Sira0909 <arisnyder27@gmail.com>
2025-03-17 21:45:50 -0400

    asdfjkl;

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
@@ -6,10 +6,15 @@ import org.firstinspires.ftc.teamcode.HardwareRobot;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 
 public class BasicTeleopForTSPMO extends LinearOpMode {
+    
+
+    HardwareRobot robot;
+    DriveSubsystem drive;
+
     @Override
     public void runOpMode () throws InterruptedException {
-        HardwareRobot robot = new HardwareRobot(hardwareMap);
-        DriveSubsystem drive = new DriveSubsystem(
+        robot = new HardwareRobot(hardwareMap);
+        drive = new DriveSubsystem(
                 robot.rightFront,
                 robot.rightBack,
                 robot.leftFront,
@@ -17,11 +22,25 @@ public class BasicTeleopForTSPMO extends LinearOpMode {
         );
         waitForStart();
         while (opModeIsActive()) {
+	        drivecommands();
+		armcommands()
+        }
+    }
+
+    public void drivecommands(){
             double speed = 1;
             double strafe = gamepad1.left_stick_x;
             double forward = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
             drive.driveRobotCentric(strafe * speed, forward * speed, turn * speed);
-        }
+    }
+    
+    public void armcommands(){
+	double speed = 1;
+	double armup = -gamepad1.right_stick_y;
+	double armopen= gamepad1.left_bumper;
+	double armclose=gamepad1.right_bumper;
+	//move claw;
+
     }
 }
