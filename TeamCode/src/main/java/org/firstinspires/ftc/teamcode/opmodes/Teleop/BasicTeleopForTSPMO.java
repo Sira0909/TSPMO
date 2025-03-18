94f68296fa4707b54a673f2b3c9a6430636c3e9b
Sira0909 <arisnyder27@gmail.com>
2025-03-18 15:53:32 -0400

    fixing viir's stuff
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
@@ -6,10 +6,15 @@ import org.firstinspires.ftc.teamcode.HardwareRobot;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 
 public class BasicTeleopForTSPMO extends LinearOpMode {
+    
+
+    HardwareRobot robot;
+    DriveSubsystem drive;
+
     @Override
     public void runOpMode () throws InterruptedException {
-        HardwareRobot robot = new HardwareRobot(hardwareMap);
-        DriveSubsystem drive = new DriveSubsystem(
+        robot = new HardwareRobot(hardwareMap);
+        drive = new DriveSubsystem(
                 robot.rightFront,
                 robot.rightBack,
                 robot.leftFront,
@@ -17,22 +22,47 @@ public class BasicTeleopForTSPMO extends LinearOpMode {
         );
         waitForStart();
         while (opModeIsActive()) {
-            double strafe = gamepad1.left_stick_x;
-            double forward = -gamepad1.right_stick_y;
-            double turn = gamepad1.right_stick_x;
-            drive.driveRobotCentric(strafe,forward,turn);
-            if (gamepad1.circle) {
-
-            }
-            if (gamepad1.triangle) {
-
-            }
-            if (gamepad1.cross) {
-
-            }
-            if (gamepad1.square) {
-
-            }
+	        drivecommands();
+		    armcommands();
+		    letterbuttons();
         }
     }
+
+    public void drivecommands(){
+            double speed = 1;
+            double strafe = gamepad1.left_stick_x;
+            double forward = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            drive.driveRobotCentric(strafe * speed, forward * speed, turn * speed);
+    }
+    
+    public void armcommands(){
+	double speed = 1;
+	double armup = -gamepad1.right_stick_y;
+	boolean armopen= gamepad1.left_bumper;
+	boolean armclose=gamepad1.right_bumper;
+	//move claw;
+
+    }
+
+    public void letterbuttons(){
+	boolean cross = gamepad1.cross;
+	boolean circle = gamepad1.circle;
+	boolean square = gamepad1.square;
+	boolean triangle = gamepad1.triangle;
+	if(cross){
+	    //macro to allign claw to backboard?
+	}
+	if(circle){
+	    //?
+	}
+	if(square){
+	    //?
+	}
+	if(triangle){
+	    //launch drone?
+	}
+    }
+    
+
 }
