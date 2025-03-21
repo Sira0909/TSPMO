4c1382e3722134783c44f488c6d7a910f6f13b41
Your Name <you@example.com>
2025-03-20 20:14:09 -0400

    reviewing arjan's changes - looks good

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
@@ -1,62 +1,111 @@
 package org.firstinspires.ftc.teamcode.opmodes.Teleop;
 
+import android.graphics.Canvas;
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.teamcode.HardwareRobot;
-import org.firstinspires.ftc.teamcode.RobotSystem;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.indepSubsystem;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
 
 public class BasicTeleopForTSPMO extends LinearOpMode {
-    
 
-    public RobotSystem robot;
+
+    HardwareRobot robot;
+    DriveSubsystem drive;
 
     @Override
     public void runOpMode () throws InterruptedException {
-        this.robot = new RobotSystem(hardwareMap,this);
+        robot = new HardwareRobot(hardwareMap);
+        drive = new DriveSubsystem(
+                robot.rightFront,
+                robot.rightBack,
+                robot.leftFront,
+                robot.leftBack
+        );
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640,480)) // place holder values ask for real size
+                .build();
+
         waitForStart();
-        boolean claw = true, toggleClaw = false;
-        boolean elbow = true, toggleElbow = false;
-        while (opModeIsActive()) {
-            driveCommands();
-            elbowCommands();
-            letterButtons();
+
+
+
+
+        while (!isStopRequested() && opModeIsActive()) {
+            drivecommands();
+            armcommands();
+            letterbuttons();
+            apriltagdetect(tagProcessor);
         }
+
+
     }
 
-    public void driveCommands(){
+    public void drivecommands(){
         double speed = 1;
         double strafe = gamepad1.left_stick_x;
         double forward = -gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
-        robot.drive.driveRobotCentric(strafe * speed, forward * speed, turn * speed);
+        drive.driveRobotCentric(strafe * speed, forward * speed, turn * speed);
     }
-    
-    public void elbowCommands(){
+
+    public void armcommands(){
         double speed = 1;
-        double armUp = -gamepad1.right_stick_y;
+        double armup = -gamepad1.right_stick_y;
+        boolean armopen= gamepad1.left_bumper;
+        boolean armclose=gamepad1.right_bumper;
+        //move claw;
 
     }
 
-    public void letterButtons(){
+    public void letterbuttons() {
         boolean cross = gamepad1.cross;
-        boolean circle = gamepad1.circle;
         boolean square = gamepad1.square;
         boolean triangle = gamepad1.triangle;
-        if(cross){
+        if (cross) {
             //macro to allign claw to backboard?
         }
-        if(circle){
-            robot.inDep.setClawPos(0);
-        }
-        if(square){
-            robot.inDep.setElbowPos(0);
+        if (square) {
             //?
         }
-        if(triangle){
+        if (triangle) {
             //launch drone?
         }
     }
-}
 
+    public void apriltagdetect(AprilTagProcessor tagProcessor) {
+        if (gamepad1.circle) {
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                telemetry.addLine("AprilTag Detected!");
+                telemetry.addLine("x: " + tag.ftcPose.x);
+                telemetry.addLine("y: " + tag.ftcPose.y);
+                telemetry.addLine("z: " + tag.ftcPose.z);
+            } else {
+                telemetry.addLine("No Tag Detected.");
+            }
+            telemetry.update();
+        }
+    }
+
+
+}
