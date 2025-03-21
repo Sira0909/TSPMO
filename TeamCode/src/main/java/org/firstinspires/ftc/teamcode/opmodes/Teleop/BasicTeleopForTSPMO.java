036766565d60d9dfdd2992b68e14b3ba96f0e9ba
Your Name <you@example.com>
2025-03-20 22:06:20 -0400

    apriltag works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/BasicTeleopForTSPMO.java
@@ -8,48 +8,54 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.teamcode.HardwareRobot;
+import org.firstinspires.ftc.teamcode.RobotSystem;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.opencv.core.Mat;
+import org.openftc.apriltag.AprilTagPose;
 
 import java.util.ArrayList;
+import java.util.List;
 
+//adding documentation and fixing apriltag stuff - viir 3/20
 public class BasicTeleopForTSPMO extends LinearOpMode {
 
-
-    HardwareRobot robot;
-    DriveSubsystem drive;
+//creates robot as object of compiled robotsystem class w all subsystems
+    public RobotSystem robot;
 
     @Override
     public void runOpMode () throws InterruptedException {
-        robot = new HardwareRobot(hardwareMap);
-        drive = new DriveSubsystem(
-                robot.rightFront,
-                robot.rightBack,
-                robot.leftFront,
-                robot.leftBack
-        );
-
-        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+        //define robot object
+        this.robot = new RobotSystem(hardwareMap, this);
+        //ofc, tweak all of these
+        double clawClosed = 0;
+        double clawOpen = 1;
+        //assuming elbow has infinite positions
+        //ari also wanted this
+        double elbowPos = gamepad1.right_stick_y;
+        //setting pos for claw and elbow
+        robot.inDep.setClawPos(clawClosed); //test servo positions once accessible
+        robot.inDep.setElbowPos(elbowPos); //test servo pos as well
+        //apriltag intialization
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder() //creates object of processor class for detection\
+                //calling set up methods - drawing and mapping out possible predicted tags
                 .setDrawAxes(true)
                 .setDrawCubeProjection(true)
                 .setDrawTagID(true)
                 .setDrawTagOutline(true)
+                //build intializes all of these
                 .build();
-
-        VisionPortal visionPortal = new VisionPortal.Builder()
+        VisionPortal visionPortal = new VisionPortal.Builder() //actual cv program built in
+                //adding processor inside of camera display to detect tags
                 .addProcessor(tagProcessor)
+                //adding cam to do the actual detection
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                //setting cam positions
                 .setCameraResolution(new Size(640,480)) // place holder values ask for real size
                 .build();
-
         waitForStart();
-
-
-
-
         while (!isStopRequested() && opModeIsActive()) {
             drivecommands();
             armcommands();
@@ -60,12 +66,12 @@ public class BasicTeleopForTSPMO extends LinearOpMode {
 
     }
 
-    public void drivecommands(){
+    public void drivecommands() {
         double speed = 1;
         double strafe = gamepad1.left_stick_x;
         double forward = -gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
-        drive.driveRobotCentric(strafe * speed, forward * speed, turn * speed);
+        robot.drive.driveRobotCentric(strafe * speed, forward * speed, turn * speed);
     }
 
     public void armcommands(){
@@ -91,21 +97,26 @@ public class BasicTeleopForTSPMO extends LinearOpMode {
             //launch drone?
         }
     }
-
+//method for detection
     public void apriltagdetect(AprilTagProcessor tagProcessor) {
         if (gamepad1.circle) {
-            if (tagProcessor.getDetections().size() > 0) {
-                AprilTagDetection tag = tagProcessor.getDetections().get(0);
-                telemetry.addLine("AprilTag Detected!");
-                telemetry.addLine("x: " + tag.ftcPose.x);
-                telemetry.addLine("y: " + tag.ftcPose.y);
-                telemetry.addLine("z: " + tag.ftcPose.z);
+            List<AprilTagDetection> detections = tagProcessor.getDetections();
+
+            if (!detections.isEmpty()) {
+                telemetry.addLine("AprilTags Detected:");
+                for (AprilTagDetection tag : detections) {
+                    telemetry.addData("Tag ID", tag.id);
+                    telemetry.addData("x", tag.ftcPose.x);
+                    telemetry.addData("y", tag.ftcPose.y);
+                    telemetry.addData("z", tag.ftcPose.z);
+                }
             } else {
                 telemetry.addLine("No Tag Detected.");
             }
             telemetry.update();
         }
     }
-
-
 }
+
+
+
