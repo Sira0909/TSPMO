6eed8552d1a71eaacb5d532191c578fb1f5aadb8
Ari Snyder <arisnyder27@gmail.com>
2025-04-21 22:07:25 -0400

    fixed

---
+++ TeamCode/src/main/java/org.firstinspire.ftc.teamcode/RobotSystem.java
@@ -0,0 +1,27 @@
+package org.firstinspire.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import teamcode.subsystems.DriveSubsystem;
+import teamcode.subsystems.LowClawSubsystem;
+import teamcode.subsystems.indepSubsystem;
+
+public class RobotSystem {
+
+    public final LinearOpMode opMode;
+    public final DriveSubsystem drive;
+    public final indepSubsystem inDep;
+    public final LowClawSubsystem lowClaw;
+
+    public RobotSystem(HardwareMap hardwareMap, LinearOpMode opMode) {
+        this.opMode = opMode;
+        HardwareRobot hardwareRobot = new HardwareRobot(hardwareMap);
+        this.drive = new DriveSubsystem(
+                hardwareRobot.leftFront,
+                hardwareRobot.rightFront,
+                hardwareRobot.leftBack,
+                hardwareRobot.rightBack
+        );
+        this.inDep = null;
+        this.lowClaw = null;
+    }
+}
---
+++ TeamCode/src/main/java/org.firstinspire.ftc.teamcode/subsystems/indepSubsystem.java
@@ -0,0 +1,43 @@
+package org.firstinspire.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.Robot;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspire.ftc.teamcode.RobotConstants;
+public class indepSubsystem extends SubsystemBase {
+    private final LinearOpMode OP_MODE;
+    private final DriveSubsystem DRIVE;
+    private final LowClawSubsystem LOWCLAW;
+    private final RobotConstants ROBOTCONSTANTS=new RobotConstants();
+    //private final CVSubsystem CV;
+
+    public indepSubsystem(
+            LinearOpMode opMode,
+            DriveSubsystem driveSubsystem,
+            LowClawSubsystem LowClaw
+            //CVSubsystem cvSubsystem
+    ) {
+        OP_MODE = opMode;
+        DRIVE = driveSubsystem;
+        LOWCLAW = LowClaw;
+        //CV = cvSubsystem;
+    }
+
+    public void setElbowPos(double ElbowPos) {
+        LOWCLAW.setElbowPos(ElbowPos);
+    }
+    public void openClaw(){
+        LOWCLAW.setClawPos(ROBOTCONSTANTS.OPENCLAW);
+    }
+    public void closeClaw(){
+        LOWCLAW.setClawPos(ROBOTCONSTANTS.CLOSECLAW);
+    }
+    public void setLiftPos(double position) {
+        LOWCLAW.setLeftLift(position);
+        LOWCLAW.setRightLift(position);
+    }
+    public double getLiftPos(){
+        return LOWCLAW.getLeftLift();
+    }
+}
