831df332031bfa2469249882cbbf36ad0bf9c55a
Your Name <you@example.com>
2025-09-06 13:09:29 -0400

    brwoejfawef

--- TeamCode/src/main/java/org.firstinspire.ftc.teamcode/cv/iwajef0ipawjef.java
+++ TeamCode/src/main/java/org.firstinspire.ftc.teamcode/cv/iwajef0ipawjef.java
@@ -1,13 +1,12 @@
 package org.firstinspire.ftc.teamcode.cv;
 
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-    public class pipelinetest extends OpenCvPipeline {
+    public class iwajef0ipawjef extends OpenCvPipeline {
         Mat mask = new Mat();
         Mat ret = new Mat();
         Scalar red = new Scalar(255, 0, 0);
@@ -16,7 +15,6 @@ import org.openftc.easyopencv.OpenCvPipeline;
             Rect r = new Rect(150, 500, 100,100);
             Rect rr = new Rect(500, 500, 100, 100);
             Rect rrr = new Rect(750, 500, 100, 100);
-            Core.bitwise_and(img, mask, ret);
             Imgproc.rectangle(img, r, red);
             Imgproc.rectangle(img, rr, red);
             Imgproc.rectangle(img, rrr, red);
